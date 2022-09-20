4876ca59c59cfd5b3d6eaee8177f37b6a7a52bb8
Fasbert <nikita.landar96@gmail.com>
2022-09-20 09:46:56 +0300

    init v2

--- script/main.js
+++ script/main.js
@@ -1,1 +1,6 @@
-const mainCanvas = document.getElementById('mainCanvas');
+window.addEventListener('load', function(){
+    const mainCanvas = this.document.getElementById('mainCanvas');
+    const ctx = mainCanvas.getContext('2d');
+    mainCanvas.width = 1280;
+    mainCanvas.height = 720;
+});
